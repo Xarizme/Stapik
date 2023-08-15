ef65ef0bca55d54de32aa3342696d383f438c67e
Xarizme <142203373+Xarizme@users.noreply.github.com>
2023-08-15 21:24:19 +0300

    Add files via upload

---
+++ ctring.java
@@ -0,0 +1,17 @@
+import java.lang.Integer;
+
+public class ctring {
+
+    public static void main(String args[]) {
+        int a = 256;
+
+        System.out.println(Integer.toBinaryString(a));
+
+        int b = Integer.bitCount(a);
+        if (b > 1 || b == 0) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
+    }
+}
