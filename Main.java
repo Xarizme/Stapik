2037b58e7cf7ec1ccc5fcd2db925da2268b3a0bb
Xarizme <142203373+Xarizme@users.noreply.github.com>
2023-08-21 20:14:19 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String a = "Madam, I'm Adam!";
-        String b = new StringBuilder(a).reverse().toString();
-        System.out.println(a.replaceAll("[^A-Za-z0-9]","").equalsIgnoreCase(b.replaceAll("[^A-Za-z0-9]","")));
-    }
-}
