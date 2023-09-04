cdc8367035012e71b30e1d45b4dfb4af9714abbd
Xarizme <142203373+Xarizme@users.noreply.github.com>
2023-09-04 12:44:12 +0300

    Add files via upload

---
+++ src/org/stepic/java/shapes/Shape.java
@@ -0,0 +1,10 @@
+package org.stepic.java.shapes;
+
+import java.awt.*;
+
+public class Shape {
+    private final Color color;
+    public Shape(Color color) {this.color=color;}
+    public Color getColor() {return color;}
+    public double getArea() {return Double.NaN;}
+}
